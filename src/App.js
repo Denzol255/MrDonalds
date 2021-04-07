0eb921fd33ad1f87806fd8951c076be915bab053
Denzol255 <den-zoloto@mail.ru>
2021-04-07 14:04:09 +0300

    Added: full task of lesson-3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { NavBar } from './Components/Navbar';
+
+const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box;
+  }
+  
+  *,
+  *::after,
+  *::before{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  body{
+    margin: 0;
+    background-color: #f0f0f0;
+    font-family: Roboto, sans-serif;
+    font-size: 20px;
+    color: black;
+  }
+  img{
+    max-width: 100%;
+    height: auto;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+  ul{
+    list-style: none;
+  }
+  h1, h2, h3{
+    font-family: Pacifico;
+  }
+  button{
+     cursor: pointer;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <NavBar />
+    </>
   );
 }
 
